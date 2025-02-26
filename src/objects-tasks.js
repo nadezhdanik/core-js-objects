6350d338abbac3c0263c767d354629491d8a1dc2
nadezhdanik <nadezhdanik@gmail.com>
2025-02-26 15:00:50 +0200

    feat: pass 14

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -76,8 +76,8 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
@@ -91,8 +91,8 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
 }
 
 /**
@@ -111,8 +111,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -125,8 +125,15 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const result = [];
+  const entries = Object.entries(lettersObject);
+  entries.forEach(([key, array]) => {
+    array.forEach((index) => {
+      result[index] = key;
+    });
+  });
+  return result.join('');
 }
 
 /**
@@ -143,8 +150,23 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  if (queue.length === 0) return true;
+
+  const ticketCost = 25;
+  let changeBank = 0;
+  let canSell;
+
+  queue.forEach((bill) => {
+    const changeNeeded = bill - ticketCost;
+    if (changeBank >= changeNeeded) {
+      canSell = true;
+      changeBank = changeBank - changeNeeded + bill;
+    } else {
+      canSell = false;
+    }
+  });
+  return canSell;
 }
 
 /**
